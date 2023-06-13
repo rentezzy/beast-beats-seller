9e344ac9cdaafc71b224adce404174aceb6ba9c1
rentezzy <ren2ren4ik@gmail.com>
2023-06-13 12:07:55 +0300

    bg image added

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,12 +1,24 @@
 import { useGetAppInfoQuery } from "../../store/slices/api/appApi";
+import styles from "./Home.module.css";
 import Ticker from "../ui/Ticker";
+import bgimage from "../../assests/homePageBG.svg";
 
 const Home = () => {
   const { data, isSuccess } = useGetAppInfoQuery(null);
 
   return (
-    <div>
+    <div className={styles.home}>
       <Ticker text={isSuccess ? data.ticker : ""} />
+      <div className="container">
+        <div className={styles.mainDiv}>
+          <img
+            src={bgimage}
+            alt=""
+            className={`${styles.mainImage} noselectText`}
+            draggable="false"
+          />
+        </div>
+      </div>
     </div>
   );
 };
