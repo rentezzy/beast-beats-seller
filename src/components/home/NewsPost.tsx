e2a501cf679f6f6a5ffe1526dba0876bebc1cb0c
rentezzy <ren2ren4ik@gmail.com>
2023-06-16 18:53:01 +0300

    News post styles

--- src/components/home/NewsPost.tsx
+++ src/components/home/NewsPost.tsx
@@ -1,13 +1,46 @@
 import { forwardRef } from "react";
 
+import { useGetMeQuery } from "../../store/slices/api/authApi";
+import { useAppSelector } from "../../store/hooks";
+
 import styles from "./Home.module.css";
 import { INewsPost } from "../../types/auth.types";
 
 const NewsPost = forwardRef<HTMLDivElement, { post: INewsPost }>(
   (props, ref) => {
+    const isLogined = useAppSelector((state) => state.appState.isLogined);
+    const { data } = useGetMeQuery(null);
+
+    let isLiked = "";
+    if (isLogined && data) {
+      isLiked = props.post.liked.includes(data._id)
+        ? styles.newsPost__like_liked
+        : "";
+    }
+
+    const date = new Date(props.post.published);
+    const dateString = `${date.getFullYear()}/${
+      date.getMonth() + 1
+    }/${date.getDay()} - ${date.getHours()}:${date.getMinutes()}`;
+
     return (
       <div ref={ref} className={styles.newsPost}>
-        {props.post.text}
+        <div className={styles.newsPost__title}>{props.post.title}</div>
+        <div className={styles.newsPost__text}>{props.post.text}</div>
+        <div className={styles.newsPost__footer}>
+          <div className={styles.newsPost__info}>
+            <div className={styles.newsPost__author}>
+              Author: {props.post.authorUsername}
+            </div>
+            <div className={styles.newsPost__date}>{dateString}</div>
+          </div>
+          <div className={styles.newsPost__likeInfo}>
+            <div className={styles.newsPost__likes}>
+              {props.post.liked.length}
+            </div>
+            <div className={`${styles.newsPost__like} ${isLiked} noselectText`}>♡</div>
+          </div>
+        </div>
       </div>
     );
   }
