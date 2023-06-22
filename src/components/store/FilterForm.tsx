4077c59c0eff065aad3a833c5b2a2b52e527584b
rentezzy <ren2ren4ik@gmail.com>
2023-06-22 11:18:47 +0300

    Filter styles

--- src/components/store/FilterForm.tsx
+++ src/components/store/FilterForm.tsx
@@ -2,7 +2,8 @@ import { Form, Formik } from "formik";
 import * as Yup from "yup";
 import { useGetAppInfoQuery } from "../../store/slices/api/appApi";
 import { useGetArtistsQuery } from "../../store/slices/api/artistsApi";
-import { MyButton, MySelect } from "../ui/Controls";
+import { MyButton, MyRange, MySelect } from "../ui/Controls";
+import styles from "./Store.module.css";
 
 const FilterForm = () => {
   const { data: artistsData } = useGetArtistsQuery(null);
@@ -12,19 +13,27 @@ const FilterForm = () => {
     artists = artistsData.map((user) => user.username);
   }
   return (
-    <div className={"hi"}>
+    <div className={styles.filterForm}>
+      <h2>Filters</h2>
       <Formik
-        initialValues={{ genre: "all", author: "all" }}
+        initialValues={{
+          genre: "all",
+          author: "all",
+          priceFrom: 0,
+          priceTo: appData?.maxPrice,
+        }}
         onSubmit={(values) => {
           console.log(values);
         }}
         validationSchema={Yup.object({
           genre: Yup.string(),
           author: Yup.string(),
+          priceFrom: Yup.number(),
+          priceTo: Yup.number(),
         })}
       >
         <Form>
-          <div>
+          <div className={styles.filterForm__selects}>
             <MySelect name="genre" type="" label="genre">
               <option value="all">All</option>
               {appData?.genres.map((genre, index) => (
@@ -42,7 +51,13 @@ const FilterForm = () => {
               ))}
             </MySelect>
           </div>
-          
+          <MyRange
+            nameFrom="priceFrom"
+            nameTo="priceTo"
+            label="price"
+            labelFrom="from:"
+            labelTo="to:"
+          />
           <MyButton type="submit">SEARCH</MyButton>
         </Form>
       </Formik>
