4f5ec97ce0b4314d4e682cd9b7c9f80ff6f07d1b
rentezzy <ren2ren4ik@gmail.com>
2023-07-04 14:33:24 +0300

    Create comment area

--- src/components/song/Song.tsx
+++ src/components/song/Song.tsx
@@ -1,9 +1,11 @@
+import { useRef } from "react";
 import { useParams } from "react-router-dom";
 import { useGetMusicQuery } from "../../store/slices/api/musicApi";
 import { useGetArtistsQuery } from "../../store/slices/api/artistsApi";
 
 import SongInfo from "./songInfo/SongInfo";
-import SongComments from "./songComments/SongComments";
+import SongFeed from "./songComments/SongFeed";
+import SongForm from "./songComments/SongForm";
 
 const Song = () => {
   const { id } = useParams();
@@ -19,7 +21,8 @@ const Song = () => {
   return (
     <div className="container">
       <SongInfo music={musicData} author={author} />
-      <SongComments />
+      <SongForm getTimestamp={() => 0} musicID={musicData._id} />
+      <SongFeed />
     </div>
   );
 };
--- src/components/song/songComments/SongForm.tsx
+++ src/components/song/songComments/SongForm.tsx
@@ -1,5 +1,57 @@
-const SongForm = () => {
-  return <div>SongForm</div>;
+import * as Yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { Form, Formik } from "formik";
+
+import styles from "../Song.module.css";
+import { MyButton, MyTextInput } from "../../ui/Controls";
+import { useCreateNewMusicCommentMutation } from "../../../store/slices/api/musicApi";
+
+interface IProps {
+  musicID: string;
+  getTimestamp: () => number;
+}
+// TODO: Timestamp function( via react context or hooks or forward ref)
+
+const SongForm: React.FC<IProps> = ({ getTimestamp, musicID }) => {
+  const [newComment, data] = useCreateNewMusicCommentMutation();
+
+  return (
+    <div className={styles.song__form}>
+      <Formik
+        initialValues={{ text: "", timestamp: false }}
+        onSubmit={(values, { resetForm }) => {
+          newComment({
+            text: values.text,
+            timestamp: values.timestamp ? getTimestamp() : 0,
+            originTo: musicID,
+          });
+          resetForm({ values: { text: "", timestamp: values.timestamp } });
+        }}
+        validationSchema={Yup.object({
+          text: Yup.string()
+            .min(3, "Min length is 3 characters")
+            .max(300, "Max length is 16 characters")
+            .required(),
+          timestamp: Yup.boolean().required(),
+        })}
+      >
+        <Form>
+          <div className={styles.song__form_comment}>
+            <div className={styles.song__form__input}>
+              <MyTextInput name="text" type="textarea" label="comment" />
+            </div>
+            <div className={styles.song__form__buttons}>
+              time?
+              <MyTextInput name="timestamp" type="checkbox"/>
+              <MyButton type="submit" disabled={data.isLoading}>
+                Send
+              </MyButton>
+            </div>
+          </div>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default SongForm;
