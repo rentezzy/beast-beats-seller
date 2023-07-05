da48e5742e8b9a0358abeffa4393c831a6b974aa
rentezzy <ren2ren4ik@gmail.com>
2023-07-05 17:43:07 +0300

    Fix styles for song comment post

--- src/components/song/songComments/SongCommentPost.tsx
+++ src/components/song/songComments/SongCommentPost.tsx
@@ -2,7 +2,7 @@ import { forwardRef } from "react";
 
 import styles from "../Song.module.css";
 import { IMusicComment } from "../../../types/auth.types";
-import { useGetTimeFromNow } from "../../../utils/utilhooks";
+import { useGetTimeAt, useGetTimeFromNow } from "../../../utils/utilhooks";
 import { useGetUserAvatar, useGetUsername } from "../../../store/hooks";
 
 interface IProps {
@@ -13,27 +13,28 @@ const SongCommentPost = forwardRef<HTMLDivElement, IProps>(
     const time = useGetTimeFromNow(comment.published);
     const avatar = useGetUserAvatar(comment.author);
     const username = useGetUsername(comment.author);
+    const at = useGetTimeAt(comment.timestamp ? comment.timestamp : 0);
 
     return (
       <div ref={ref} className={styles.song__comment__post}>
         <div className={styles.song__comment__image}>
-          <img src={avatar} alt="" />
+          <img src={avatar} className="noselectText" alt="" draggable="false" />
         </div>
         <div className={styles.song__comment__info}>
           <div className={styles.song__comment__title}>
             <div className={styles.song__comment__subtitle}>
               <div className={styles.song__comment__subtitle__author}>
-                {username},
+                {username}
               </div>
-              <div className={styles.song__comment__subtitle__at}>
-                at {comment.timestamp}
-              </div>
+              <div className={styles.song__comment__subtitle__at}>{at}</div>
             </div>
             <div className={styles.song__comment__title__time}>{time}</div>
           </div>
           <div className={styles.song__comment__section}>
             <div className={styles.song__comment__text}>{comment.text}</div>
-            <div className={styles.song__comment__like}>♡</div>
+            <div className={styles.song__comment__like + " noselectText"}>
+              ♡
+            </div>
           </div>
         </div>
       </div>
