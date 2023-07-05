f2d134d66531de6cce7af82d7a51da64735142d8
rentezzy <ren2ren4ik@gmail.com>
2023-07-05 11:44:16 +0300

    Fix react error:
    - "Warning: Cannot update a component while rendering a different component"
    - React error while dispatching actions after every rerender, wrap
    into useEffect hook.

--- src/components/song/Song.tsx
+++ src/components/song/Song.tsx
@@ -1,20 +1,23 @@
-import { useRef } from "react";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useGetMusicQuery } from "../../store/slices/api/musicApi";
 import { useGetArtistsQuery } from "../../store/slices/api/artistsApi";
+import { useActions } from "../../store/hooks";
 
 import SongInfo from "./songInfo/SongInfo";
 import SongFeed from "./songComments/SongCommentsFeed";
 import SongForm from "./songComments/SongForm";
-import { useActions } from "../../store/hooks";
 
 const Song = () => {
   const { id } = useParams();
   const { data: musicData } = useGetMusicQuery(id!);
   const { data: artistsData } = useGetArtistsQuery(null);
   const { newSong } = useActions();
+  useEffect(() => {
+    newSong(id!);
+  }, [id, newSong]);
+
   if (!musicData) return <></>;
-  newSong(musicData._id);
   let author: string | undefined = "author";
   if (artistsData) {
     author = artistsData.find(
