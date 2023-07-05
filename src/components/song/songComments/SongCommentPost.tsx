d1a506accf23b9fc56bf48c1e77df7b597610b5a
rentezzy <ren2ren4ik@gmail.com>
2023-07-05 17:10:07 +0300

    Add styles for comment post

--- src/components/song/songComments/SongCommentPost.tsx
+++ src/components/song/songComments/SongCommentPost.tsx
@@ -1,19 +1,41 @@
 import { forwardRef } from "react";
+
 import styles from "../Song.module.css";
 import { IMusicComment } from "../../../types/auth.types";
-import dayjs from "dayjs";
-import * as relativeTime from "dayjs/plugin/relativeTime";
+import { useGetTimeFromNow } from "../../../utils/utilhooks";
+import { useGetUserAvatar, useGetUsername } from "../../../store/hooks";
 
 interface IProps {
   comment: IMusicComment;
 }
 const SongCommentPost = forwardRef<HTMLDivElement, IProps>(
   ({ comment }, ref) => {
-    dayjs.extend(relativeTime.default);
-    console.log(dayjs(comment.published).fromNow());
+    const time = useGetTimeFromNow(comment.published);
+    const avatar = useGetUserAvatar(comment.author);
+    const username = useGetUsername(comment.author);
+
     return (
       <div ref={ref} className={styles.song__comment__post}>
-        {comment.text}
+        <div className={styles.song__comment__image}>
+          <img src={avatar} alt="" />
+        </div>
+        <div className={styles.song__comment__info}>
+          <div className={styles.song__comment__title}>
+            <div className={styles.song__comment__subtitle}>
+              <div className={styles.song__comment__subtitle__author}>
+                {username},
+              </div>
+              <div className={styles.song__comment__subtitle__at}>
+                at {comment.timestamp}
+              </div>
+            </div>
+            <div className={styles.song__comment__title__time}>{time}</div>
+          </div>
+          <div className={styles.song__comment__section}>
+            <div className={styles.song__comment__text}>{comment.text}</div>
+            <div className={styles.song__comment__like}>♡</div>
+          </div>
+        </div>
       </div>
     );
   }
