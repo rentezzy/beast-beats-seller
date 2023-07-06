beb6f72c6ce7c5e5b1abd1ac2d0b61dd1170a175
rentezzy <ren2ren4ik@gmail.com>
2023-07-06 23:40:05 +0300

    Add artist api

--- src/store/slices/api/artistsApi.ts
+++ src/store/slices/api/artistsApi.ts
@@ -1,5 +1,5 @@
 import { api } from "../api";
-import { ILoginUser } from "../../../types/auth.types";
+import { IArtist, ILoginUser } from "../../../types/auth.types";
 
 export const artistsApi = api.injectEndpoints({
   endpoints: (builder) => ({
@@ -9,6 +9,22 @@ export const artistsApi = api.injectEndpoints({
         return res.data.users;
       },
     }),
+    getArtistsFull: builder.query<IArtist[], any>({
+      query: () => "artist",
+      transformResponse: (res: { data: { artists: IArtist[] } }) => {
+        return res.data.artists;
+      },
+    }),
+    getArtistFull: builder.query<IArtist, string>({
+      query: (id) => `artist/${id}`,
+      transformResponse: (res: { data: { artist: IArtist } }) => {
+        return res.data.artist;
+      },
+    }),
   }),
 });
-export const { useGetArtistsQuery } = artistsApi;
+export const {
+  useGetArtistsQuery,
+  useGetArtistFullQuery,
+  useLazyGetArtistFullQuery,
+} = artistsApi;
