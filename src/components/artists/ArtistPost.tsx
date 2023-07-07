e4106bdd8ac4754ecdee4ef88d021c68efba0116
rentezzy <ren2ren4ik@gmail.com>
2023-07-07 20:27:28 +0300

    Add styles, profile redirect

--- src/components/artists/ArtistPost.tsx
+++ src/components/artists/ArtistPost.tsx
@@ -1,6 +1,9 @@
-import { useRedirectToStore } from "../../store/hooks";
+import { useGetUsername, useRedirectToStore } from "../../store/hooks";
+
 import styles from "./Artists.module.css";
 import { IArtist } from "../../types/auth.types";
+import { MyButton } from "../ui/Controls";
+import { useNavigate } from "react-router-dom";
 
 interface IProps {
   artist: IArtist;
@@ -8,19 +11,25 @@ interface IProps {
 }
 
 const ArtistPost: React.FC<IProps> = ({ artist, pos }) => {
-  const redirectHandler = useRedirectToStore(artist.user);
+  const musicHandler = useRedirectToStore(artist.user);
+  const username = useGetUsername(artist.user);
+  const navigate = useNavigate();
 
+  const profileHandler = () => navigate(`/artist/${artist.user}`);
   return (
     <div className={styles.artist__post}>
       {pos !== 0 && <ArtistImage image={artist.avatar.big} />}
       <div className={styles.artist__about}>
+        <div className={styles.artist__title}>
+          <h2>{username}</h2>
+        </div>
         <div className={styles.artist__text}>{artist.about}</div>
         <div className={styles.artist__buttons}>
           <div>
-            <button onClick={redirectHandler}>music</button>
+            <MyButton onClick={musicHandler}>MUSIC</MyButton>
           </div>
           <div>
-            <button>profile</button>
+            <MyButton onClick={profileHandler}>PROFILE</MyButton>
           </div>
         </div>
       </div>
@@ -33,9 +42,10 @@ interface IImageProps {
   image: string;
 }
 const ArtistImage: React.FC<IImageProps> = ({ image }) => {
+  const src = `${process.env.REACT_APP_MAIN_API}images/artists/big/${image}`;
   return (
     <div className={styles.artist__image}>
-      <img src={image} alt="" />
+      <img src={src} alt="" />
     </div>
   );
 };
