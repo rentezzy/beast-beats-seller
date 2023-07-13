1080077c99f77e26513cf4654d1c1a53037a5f6e
rentezzy <ren2ren4ik@gmail.com>
2023-07-13 20:25:28 +0300

    Add entry file and main form

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,27 +1,10 @@
+import CartForm from "./CartForm";
+
 const Cart = () => {
   return (
     <div className="container">
-      <form
-        method="POST"
-        action="https://www.liqpay.ua/api/3/checkout"
-        accept-charset="utf-8"
-      >
-        <input
-          type="hidden"
-          name="data"
-          value="eyJ2ZXJzaW9uIjozLCJwdWJsaWNfa2V5Ijoic2FuZGJveF9pMjAwMDE3NzIzMCIsImFjdGlvbiI6InBheSIsImFtb3VudCI6NSwiY3VycmVuY3kiOiJVU0QiLCJkZXNjcmlwdGlvbiI6IkZvciBiZWF0cyIsInByb2R1Y3RfZGVzY3JpcHRpb24iOiJIZWxsbyIsIm9yZGVyX2lkIjoiYXNsZGFzZCIsInJyb19pbmZvIjp7Iml0ZW1zIjpbeyJhbW91bnQiOjEsInByaWNlIjoyMDIsImNvc3QiOjIwMiwiaWQiOjEyMzQ1Nn1dfSwicHJvZHVjdF9uYW1lIjoiQkVBVCJ9"
-        />
-        <input
-          type="hidden"
-          name="signature"
-          value="ElmJ0c4oBmfIYTkcxj/xx5ft1Dw="
-        />
-        <input
-          type="image"
-          src="//static.liqpay.ua/buttons/p1ru.radius.png"
-          alt=""
-        />
-      </form>
+      <h1>cart</h1>
+      <CartForm />
     </div>
   );
 };
