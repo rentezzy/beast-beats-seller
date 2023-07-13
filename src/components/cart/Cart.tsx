90da99643bc363a05cb7c5b5bdc39b702c372903
rentezzy <ren2ren4ik@gmail.com>
2023-07-13 20:32:24 +0300

    Fix cart page

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,9 +1,10 @@
+import styles from "./Cart.module.css";
 import CartForm from "./CartForm";
 
 const Cart = () => {
   return (
     <div className="container">
-      <h1>cart</h1>
+      <h1 className={styles.cart__title}>cart</h1>
       <CartForm />
     </div>
   );
