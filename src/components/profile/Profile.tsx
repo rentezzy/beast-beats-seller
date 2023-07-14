7d028bd3a044dd40178d7fce771a9bfd33b46ecb
rentezzy <ren2ren4ik@gmail.com>
2023-07-14 16:18:38 +0300

    Add sidebar navigation with styles

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,8 +1,21 @@
-import { Outlet } from "react-router-dom";
-
+import { NavLink, Outlet } from "react-router-dom";
+import styles from "./Profile.module.css";
 const Profile = () => {
+  const styleName = ({ isActive }: { isActive: boolean }) => {
+    return isActive
+      ? `${styles.linkNavActive} ${styles.linkNav}`
+      : styles.linkNav;
+  };
   return (
-    <div>
+    <div className={`${styles.profile} container`}>
+      <nav className={styles.profile__navigation}>
+        <NavLink to={"settings"} className={styleName}>
+          SETTINGS
+        </NavLink>
+        <NavLink to={"security"} className={styleName}>
+          SECURITY
+        </NavLink>
+      </nav>
       <Outlet />
     </div>
   );
